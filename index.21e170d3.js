62c799cd053fd219b77fcb00bc0b6da49ba9cadf
PokhilenkoD <107175791+PokhilenkoD@users.noreply.github.com>
2022-08-26 13:49:11 +0000

    Deploying to gh-pages from @ PokhilenkoD/food-delivery-service_project-16@a5108a9741430d9e322c704fb8b5b4a69d426055 🚀

---
+++ index.21e170d3.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.21e170d3.js.map
